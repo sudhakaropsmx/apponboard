0df2cb2cd7145092acabc45d3fb0434a8c152509
sudhakaropsmx <sudhakar@opsmx.in>
2019-08-20 10:13:11 +0530

    Added Initial Commit

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
    
-    //"encoding/json"
+    "encoding/json"
     "fmt"
     "log"
     //"io/ioutil"
     "net/http"
     "github.com/gorilla/mux"
      postgresdb "github.com/sudhakaropsmx/apponboard/models"
+     
 )
 
 
@@ -30,16 +31,64 @@ func getGroups(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, postgresdb.GetGroupsData())
 	fmt.Println("Endpoint Hit: GroupsAPI")
 }
-
-func getAppGroups(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, postgresdb.getAppGroupsData())
-	fmt.Println("Endpoint Hit: GroupsAPI")
+type myAppData struct {
+	Application string
 }
+func getAppGroups(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+       http.Error(w, http.StatusText(405), 405)
+      return
+   }
+	decoder := json.NewDecoder(r.Body)
+
+	var data myAppData
+	err := decoder.Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+	application := data.Application
+	fmt.Fprintf(w, postgresdb.GetAppGroupsData(application))
+	fmt.Println("Endpoint Hit: AppGroupsAPI")
+}
+func getUserGroups(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, postgresdb.GetUserGroupsData("opsmxemp1"))
+	fmt.Println("Endpoint Hit: UserGroupsAPI")
+}
+type myData struct {
+	User string
+	Groups  []string
+}
+
+func GetUserAuthorized(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+       http.Error(w, http.StatusText(405), 405)
+      return
+   }
+	decoder := json.NewDecoder(r.Body)
+
+	var data myData
+	err := decoder.Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+	groups := data.Groups
+	
+	userdata := postgresdb.GetUserAuthorizedData(data.User,groups)
+	if err != nil {
+        http.Error(w, http.StatusText(500), 500)
+        return
+    }
+	fmt.Fprintf(w, userdata )
+	fmt.Println("Endpoint Hit: GetUserAuthorizedAPI")
+}
+
 func handleRequests() {
 	postgresdb.InitDB()
     myRouter := mux.NewRouter().StrictSlash(true)
     myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/api/getAppAuthorization", getAppGroups)
+    myRouter.HandleFunc("/api/getAppAuthorized",GetUserAuthorized).Methods("POST")
+    myRouter.HandleFunc("/api/getAppGroups", getAppGroups).Methods("POST")
+    myRouter.HandleFunc("/api/getUserGroups", getUserGroups).Methods("POST")
     myRouter.HandleFunc("/api/users", getUsers)
     myRouter.HandleFunc("/api/applications", getApplications)
     myRouter.HandleFunc("/api/groups", getGroups)
--- models/postgres.go
+++ models/postgres.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"encoding/json"
   _ "github.com/lib/pq"
+  "strings"
 )
 
 // repository contains the details of a repository
@@ -19,8 +20,12 @@ type Users struct {
 	Use_Id       int
 	UserName     string
 }
-type AppGroup struct {
+type AppGroups struct {
 	Application       string
+	GroupName     string
+}
+type UserGroups struct {
+	UserName       string
 	GroupName     string
 }
 
@@ -33,33 +38,104 @@ type repositoriesUser struct {
 type repositoriesGroup struct {
 	GroupRepositories []Groups
 }
-type repositoriesAppGroup struct {
-	AppGroupRepositories []AppGroup
+type repositoriesAppGroups struct {
+	AppGroupRepositories []AppGroups
 }
-func GetAppGroupsData(application string) {
-	repos := repositoriesAppGroup{}
-    rows, err := db.Query(`SELECT application.application_name,groups.group_name  
-       FROM application,application_groups,groups WHERE application_groups.app_id = application.app_id 
-        AND application_groups.group_id = groups.group_id AND application.application_name=?
-    `,application)
+type repositoriesUserGroups struct {
+	UserGroupRepositories []UserGroups
+}
+func GetAppGroupsData(application string) string {
+	 
+	repos := repositoriesAppGroups{}
+	sql := "SELECT application.application_name,groups.group_name "+  
+       " FROM application,application_groups,groups WHERE application_groups.app_id = application.app_id "+
+       " AND application_groups.group_id = groups.group_id AND application.application_name = $1"
+   // fmt.Printf(sql)   
+    rows, err := db.Query(sql,&application)
 	if err != nil {
 		fmt.Println("Query Error :",err)
+		
 	}
 	defer rows.Close()
 	for rows.Next() {
-		repo := AppGroup{}
+		repo := AppGroups{}
 		err := rows.Scan(
 			&repo.Application,
 			&repo.GroupName, 
 		)
 		if err != nil {
 			fmt.Println("Error encountered fetch data",err)
+		
 		}
 		repos.AppGroupRepositories = append(repos.AppGroupRepositories, repo)
 	 }	
 	 out, err := json.Marshal(repos)
 	 if err != nil {
+	  panic(err)
+	 }
+     fmt.Println(string(out))
+     
+     return string(out)
+}	
+func GetUserGroupsData(username string) string {
+	
+	repos := repositoriesUserGroups{}
+	sql := "SELECT user_table.username,groups.group_name "+  
+       " FROM user_table,user_groups,groups WHERE user_groups.user_id = user_table.user_id "+
+       " AND user_groups.group_id = groups.group_id AND user_table.username = $1"
+   // fmt.Printf(sql)   
+    rows, err := db.Query(sql,&username)
+	if err != nil {
+		fmt.Println("Query Error :",err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		repo := UserGroups{}
+		err := rows.Scan(
+			&repo.UserName,
+			&repo.GroupName, 
+		)
+		if err != nil {
+			fmt.Println("Error encountered fetch data",err)           
+		}
+		repos.UserGroupRepositories = append(repos.UserGroupRepositories, repo)
+	 }	
+	 out, err := json.Marshal(repos)
+	 if err != nil {
 	    panic(err)
+	 }
+     fmt.Println(string(out))
+     
+     return string(out)
+}	
+func GetUserAuthorizedData(username string,groups []string) string {
+	
+	repos := repositoriesUserGroups{}
+	group_params := strings.Join(groups, "','")
+	sql := "SELECT user_table.username,groups.group_name "+  
+       " FROM user_table,user_groups,groups WHERE user_groups.user_id = user_table.user_id "+
+       " AND user_groups.group_id = groups.group_id AND user_table.username = $1 AND groups.group_name IN ('%s') "
+       fmt.Printf(sql)   
+    sqlRaw := fmt.Sprintf(sql, group_params)
+	rows, err := db.Query(sqlRaw, &username)    
+	if err != nil {
+		fmt.Println("Query Error :",err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		repo := UserGroups{}
+		err := rows.Scan(
+			&repo.UserName,
+			&repo.GroupName, 
+		)
+		if err != nil {
+			fmt.Println("Error encountered fetch data",err)
+		}
+		repos.UserGroupRepositories = append(repos.UserGroupRepositories, repo)
+	 }	
+	 out, err := json.Marshal(repos)
+	 if err != nil {
+	  panic(err)
 	 }
      fmt.Println(string(out))
      
@@ -75,6 +151,7 @@ func  GetUsersData() string{
 		FROM user_table` )
 	if err != nil {
 		fmt.Println("Query Error :",err)
+		
 	}
 	defer rows.Close()
 	for rows.Next() {
@@ -85,12 +162,13 @@ func  GetUsersData() string{
 		)
 		if err != nil {
 			fmt.Println("Error encountered fetch data",err)
+			
 		}
 		repos.UserRepositories = append(repos.UserRepositories, repo)
 	 }	
 	 out, err := json.Marshal(repos)
 	 if err != nil {
-	    panic(err)
+	   panic(err)
 	 }
      fmt.Println(string(out))
      
@@ -128,12 +206,12 @@ func  GetApplicationsData() string{
 }
 func  GetGroupsData() string{
 	
- repos := repositoriesGroup{}
+  repos := repositoriesGroup{}
   rows, err := db.Query(`
 		SELECT
 			group_id,
 			group_name
-		FROM group` )
+		FROM groups` )
 	if err != nil {
 		fmt.Println("Query Error :",err)
 	}
