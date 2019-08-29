aaf21dab7e65c7863d02737bb5988f423f5ec98f
Sudhakar Guvvalapall <sudhakar@opsmx.in>
2019-08-29 17:13:57 +0530

    Update postgres.go
    Added new queries

--- models/postgres.go
+++ models/postgres.go
@@ -28,6 +28,11 @@ type UserGroups struct {
 	UserName       string
 	GroupName     string
 }
+type UserAppGroups struct {
+	UserName string
+	Application string
+	Groups   []string
+}
 
 type repositoriesApplication struct {
 	AppRepositories []Applications
@@ -44,8 +49,44 @@ type repositoriesAppGroups struct {
 type repositoriesUserGroups struct {
 	UserGroupRepositories []UserGroups
 }
-func GetAppGroupsData(application string) string {
+func GetUserAppGroupsData(username string, application string) string {
 	 
+	repos := UserAppGroups{}
+	repos.Application = application
+	repos.UserName = username
+	sql := "SELECT groups.group_name FROM  "+
+	       " application,user_table,user_groups,groups,application_groups "+
+	       " WHERE user_groups.user_id = user_table.user_id AND application_groups .app_id =application.app_id "+
+	       " AND user_groups.group_id = groups.group_id  AND application_groups.group_id = groups.group_id  "+
+	       " AND user_table.username = $1 AND application.application_name = $2"
+   // fmt.Printf(sql)   
+    rows, err := db.Query(sql,&username,&application)
+	if err != nil {
+		fmt.Println("Query Error :",err)
+		
+	}
+	defer rows.Close()
+	groups := make([]string, 0)
+	for rows.Next() {
+		var group string
+		err := rows.Scan(&group)
+		if err != nil {
+			fmt.Println("Error encountered fetch data",err)
+		
+		}
+		groups = append(groups, group)
+	 }	
+	 repos.Groups = groups
+	 out, err := json.Marshal(repos)
+	 if err != nil {
+	  panic(err)
+	 }
+     fmt.Println(string(out))
+     
+     return string(out)
+}	
+func GetAppGroupsData(application string) string {
+	
 	repos := repositoriesAppGroups{}
 	sql := "SELECT application.application_name,groups.group_name "+  
        " FROM application,application_groups,groups WHERE application_groups.app_id = application.app_id "+
@@ -54,7 +95,6 @@ func GetAppGroupsData(application string) string {
     rows, err := db.Query(sql,&application)
 	if err != nil {
 		fmt.Println("Query Error :",err)
-		
 	}
 	defer rows.Close()
 	for rows.Next() {
@@ -64,14 +104,13 @@ func GetAppGroupsData(application string) string {
 			&repo.GroupName, 
 		)
 		if err != nil {
-			fmt.Println("Error encountered fetch data",err)
-		
+			fmt.Println("Error encountered fetch data",err)           
 		}
 		repos.AppGroupRepositories = append(repos.AppGroupRepositories, repo)
 	 }	
 	 out, err := json.Marshal(repos)
 	 if err != nil {
-	  panic(err)
+	    panic(err)
 	 }
      fmt.Println(string(out))
      
@@ -108,6 +147,7 @@ func GetUserGroupsData(username string) string {
      
      return string(out)
 }	
+
 func GetUserAuthorizedData(username string,groups []string) string {
 	
 	repos := repositoriesUserGroups{}
